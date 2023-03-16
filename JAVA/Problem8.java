d7c32656ef2703ee0e578c4cf2a7c384f1377966
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-03-16 21:40:04 +0530

    Problem no. Leetcode

---
+++ JAVA/Problem8.java
@@ -0,0 +1,14 @@
+package JAVA;
+import java.util.*;
+
+public class Problem8 {
+    public static void main(String[] args) {
+        String s="42";
+    }
+
+    public int myAtoi(String s) {
+        int n=0;
+        return n;
+    }
+
+}
