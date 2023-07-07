fd1af5d180ac0ea555d3e154a92174e83169bfd6
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-07-07 17:27:17 +0530

    leetcode problem number 34

--- JAVA Solutions/LeetCode/FirstLastPosition.java
+++ JAVA Solutions/LeetCode/FirstLastPosition.java
@@ -1,4 +1,4 @@
-## 34. Find First and Last Position of Element in Sorted Array
+// ## 34. Find First and Last Position of Element in Sorted Array ##
 
 class Solution {
     public int[] searchRange(int[] nums, int target) {
