77316e1f229fb47516888e16d212d52e08044a92
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-07-20 23:59:11 +0530

    volume control codechef problem

---
+++ JAVA Solutions/CodeChef/VolumeControl.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner read=new Scanner(System.in);
+		int n=read.nextInt();
+		while(n>0){
+		    int X=read.nextInt();
+		    int Y=read.nextInt();
+		    
+		    if(X>=Y) System.out.println(X-Y);
+		    else if(X<=Y) System.out.println(Y-X);
+		    --n;
+		}
+	}
+}
