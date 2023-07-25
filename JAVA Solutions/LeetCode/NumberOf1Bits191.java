6b55b62a5869a98d24c41bbce8b9f6b648226d7b
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-07-25 23:45:47 +0530

    leetcode 191 solution

---
+++ JAVA Solutions/LeetCode/NumberOf1Bits191.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int hammingWeight(int n) {
+      int ans = 0;
+      for (int i = 0; i < 32; ++i)
+        if (((n >> i) & 1) == 1){
+          ++ans;
+        }
+      return ans;
+    }
+};
+public class NumberOf1Bits191{
+    public static void main(String[] args) {
+        int n=00000000000000000000000000001011;
+        Solution s1 = new Solution();
+        int res= s1.hammingWeight(n);
+        System.out.println(res);
+    }
+}
